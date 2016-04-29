f596430522f9b5866fd4ed93150016068d680997
surew963 <surew963@gmail.com>
2016-04-29 15:29:31 -0400

    ha

---
+++ alg/implementation/angryProfessor.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        int k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for(int a_i = 0;a_i < n;a_i++){
+            cin >> a[a_i];
+            if(a[a_i]<=0)k--;
+        }
+        if(k<=0)cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+    return 0;
+}
